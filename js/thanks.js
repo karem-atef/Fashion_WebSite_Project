b47f08026199bf19ae491d85573fafc54db97128
karem941 <karematef941@gmail.com>
2021-06-09 12:09:02 +0200

    final 1

---
+++ js/thanks.js
@@ -0,0 +1,11 @@
+window.alert("Have Fun.");
+function checkFormEntry ()
+{     
+ window.location.href="ownHome.html";
+	
+		
+	
+	return false;
+}
+
+document.getElementById("form1").onsubmit=checkFormEntry;	
